831ab03676758d4c12cf72d6f4419bb3e2c65383
Frank Jordan Villachica Gaspar <extfvillachica@belcorp.biz>
2024-11-29 13:42:03 -0500

    Adding dropdown menu in profile avatar

--- app/shared/layouts/components/ProfileButtons.tsx
+++ app/shared/layouts/components/ProfileButtons.tsx
@@ -1,8 +1,16 @@
-import { Link } from '@remix-run/react';
-import { LogOut } from 'lucide-react';
+import { Link } from "@remix-run/react";
+import { LogOut } from "lucide-react";
 
-import { Avatar, AvatarFallback, AvatarImage } from '@ui/avatar';
-import { Button } from '@ui/button';
+import { Avatar, AvatarFallback, AvatarImage } from "@ui/avatar";
+import { Button } from "@ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "~/components/ui/dropdown-menu";
 
 const ProfileButtons = () => {
   return (
@@ -12,12 +20,33 @@ const ProfileButtons = () => {
           <h5 className="font-semibold text-sm">Pepita Juárez</h5>
           <p className="text-muted-foreground text-xs">Postulante</p>
         </div>
-        <Link to="/profile">
-          <Avatar className="w-9 h-9">
-            <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
-            <AvatarFallback>CN</AvatarFallback>
-          </Avatar>
-        </Link>
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="ghost" className="relative h-8 w-8 rounded-full">
+              <Avatar className="w-9 h-9">
+                <AvatarImage
+                  src="https://github.com/shadcn.png"
+                  alt="@username"
+                />
+                <AvatarFallback>CN</AvatarFallback>
+              </Avatar>
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent className="w-56" align="end" forceMount>
+            <DropdownMenuLabel className="font-normal">
+              <div className="flex flex-col space-y-1">
+                <p className="text-sm font-medium leading-none">email</p>
+                <p className="text-xs leading-none text-muted-foreground">
+                  user@example.com
+                </p>
+              </div>
+            </DropdownMenuLabel>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem asChild>
+              <Link to="/profile" className="cursor-pointer">Profile</Link>
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
       </div>
       <Button
         variant="ghost"
