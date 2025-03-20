7dcebde5506cee012c0512f429febf6e5390d0af
Frank Jordan Villachica Gaspar <extfvillachica@belcorp.biz>
2025-03-20 11:42:01 -0500

    FEAT (Favicon y logo) Se agregó el favicon de tismart asi como el logo en el header.

--- app/shared/layouts/Header.tsx
+++ app/shared/layouts/Header.tsx
@@ -9,7 +9,9 @@ const Header = ({ user }: HeaderProps) => {
     <header className="sticky top-0 z-50 w-full border-b border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60 dark:border-border">
       <div className="flex justify-between items-center px-4 py-3 max-w-[1536px] xl:mx-auto">
         <h3 className="text-2xl font-bold">
-          <Link to="/">R&S</Link>
+          <Link to="/">
+            <img src="/tismart-logo.png" alt="Logo Tismart" className="h-9" />
+          </Link>
         </h3>
         <div className="flex gap-3">
           <Button asChild variant="ghost">
@@ -23,4 +25,3 @@ const Header = ({ user }: HeaderProps) => {
 };
 
 export default Header;
-
