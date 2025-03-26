7be2bfe86ac2486b39797031778b9a355729b909
Frank Jordan Villachica Gaspar <extfvillachica@belcorp.biz>
2025-03-26 16:09:12 -0500

    ✨ (Applicant data): Se aumentaron las opciones para los multiselect de tecnologías y perfiles de interes.

--- app/modules/applicant-data/data/technologies-data.ts
+++ app/modules/applicant-data/data/technologies-data.ts
@@ -1,30 +1,210 @@
 export const technologiesList = [
   {
-    value: "react",
-    label: "React",
+    value: "vuejs",
+    label: "Vue.js",
   },
   {
     value: "angular",
     label: "Angular",
   },
   {
+    value: "react",
+    label: "React",
+  },
+  {
+    value: "nestjs",
+    label: "Nestjs",
+  },
+  {
+    value: "expressjs",
+    label: "Express.js",
+  },
+  {
     value: "nodejs",
-    label: "Node js",
+    label: "Node.js",
+  },
+  {
+    value: "nextjs",
+    label: "Next.js",
+  },
+  {
+    value: "nuxtjs",
+    label: "Nuxt.js",
+  },
+  {
+    value: "django",
+    label: "Django",
+  },
+  {
+    value: "flask",
+    label: "Flask",
+  },
+  {
+    value: "fastapi",
+    label: "FastAPI",
   },
   {
     value: "springboot",
-    label: "Springboot",
+    label: "Spring Boot",
   },
   {
-    value: "net",
-    label: "Net",
+    value: "androidsdk",
+    label: "Android SDK",
   },
   {
-    value: "aws",
-    label: "Aws"
+    value: "netcore",
+    label: ".NET Core",
+  },
+  {
+    value: "laravel",
+    label: "Laravel",
+  },
+  {
+    value: "wordpress",
+    label: "WordPress",
+  },
+  {
+    value: "swift",
+    label: "Swift",
+  },
+  {
+    value: "jetpackcompose",
+    label: "Jetpack Compose",
+  },
+  {
+    value: "kotlin",
+    label: "Kotlin",
+  },
+  {
+    value: "flutter",
+    label: "Flutter",
+  },
+  {
+    value: "mysql",
+    label: "MySQL",
+  },
+  {
+    value: "postgresql",
+    label: "PostgreSQL",
+  },
+  {
+    value: "sqlserver",
+    label: "SQL Server",
+  },
+  {
+    value: "oracledb",
+    label: "Oracle DB",
+  },
+  {
+    value: "mariadb",
+    label: "MariaDB",
+  },
+  {
+    value: "sqlite",
+    label: "SQLite",
+  },
+  {
+    value: "mongodb",
+    label: "MongoDB",
+  },
+  {
+    value: "firebase",
+    label: "Firebase",
+  },
+  {
+    value: "dynamodb",
+    label: "DynamoDB",
+  },
+  {
+    value: "cassandra",
+    label: "Cassandra",
+  },
+  {
+    value: "jira",
+    label: "Jira",
+  },
+  {
+    value: "testrail",
+    label: "TestRail",
+  },
+  {
+    value: "zephyr",
+    label: "Zephyr",
+  },
+  {
+    value: "testlink",
+    label: "TestLink",
+  },
+  {
+    value: "confluence",
+    label: "Confluence",
+  },
+  {
+    value: "notion",
+    label: "Notion",
+  },
+  {
+    value: "metagiles",
+    label: "Met. Ágiles",
   },
   {
     value: "python",
-    label: "Python"
-  }
+    label: "Python",
+  },
+  {
+    value: "jmeter",
+    label: "Jmeter",
+  },
+  {
+    value: "jenkins",
+    label: "Jenkins",
+  },
+  {
+    value: "gittools",
+    label: "Git Tools",
+  },
+  {
+    value: "cicd",
+    label: "CI/CD",
+  },
+  {
+    value: "postman",
+    label: "Postman",
+  },
+  {
+    value: "soapui",
+    label: "SoapUI",
+  },
+  {
+    value: "appium",
+    label: "Appium",
+  },
+  {
+    value: "espresso",
+    label: "Espresso",
+  },
+  {
+    value: "webdriver",
+    label: "WebDriver",
+  },
+  {
+    value: "selenium",
+    label: "Selenium",
+  },
+  {
+    value: "cypress",
+    label: "Cypress",
+  },
+  {
+    value: "aws",
+    label: "AWS",
+  },
+  {
+    value: "azure",
+    label: "Azure",
+  },
+  {
+    value: "gcp",
+    label: "GCP",
+  },
 ];
